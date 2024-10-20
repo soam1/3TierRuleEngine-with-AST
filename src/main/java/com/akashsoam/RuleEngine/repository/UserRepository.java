a3c1fd04dc77ff12d5c6d497624188f90bd66c50
soam1 <soamakash9@gmail.com>
2024-10-20 15:30:09 +0530

    removing redundant files

--- src/main/java/com/akashsoam/RuleEngine/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akashsoam.RuleEngine.repository;
-
-import com.akashsoam.RuleEngine.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
